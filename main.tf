bd0c0a696cf526816921b0ec92cd3e26681b0b5d
Nasrat Sherzad <purifiedg@gmail.com>
2026-06-01 14:44:03 +0100

    simple terraform/aws api call changes

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+#provider with AWS region and profile
+provider "aws" {
+  region = "eu-west-2"
+  profile = "sivka"
+}
+
+#resource to create an EC2 instance
 resource "aws_instance" "my_server" {
   ami           = "ami-0150189e4c09ffab5"
   instance_type = "t2.micro"
-
   tags = {
     Name = "TerraformServer"
   }
+}
+
+resource "vpc" "my_testvpc" {
+  cidr_block = "10.0.0.0/16"
 }
--- provider.tf
+++
@@ -1,4 +0,0 @@
-#provider with AWS region and profile
-provider "aws" {
-  region = "eu-west-2"
-}
