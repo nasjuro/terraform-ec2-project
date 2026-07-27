2cafd06d37441e43807eca5267c5bd3d92edb746
Nasrat Sherzad <nasrat5@proton.me>
2026-07-27 20:11:33 +0100

    HandsOn1 Complete, creating 3 resources, vpc/subnet/bucket

--- hands_on_1.tf
+++ hands_on_1.tf
@@ -1,21 +1,35 @@
-#provider with AWS region and profile
+/*
+1. create a VPC in eu-west-2, CIDR 10.0.0.0/16
+2. create a subnet with th eCIDR 10.0.2.0/24 into the above VPC
+3. create an S3 bucket in the same region
+*/
+
 provider "aws" {
   region = "eu-west-2"
-  profile = "sivka"
+  profile = "default"
 }
 
-#resource to create an EC2 instance
-resource "aws_instance" "my_server" {
-  ami           = "ami-0150189e4c09ffab5"
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+
   tags = {
-    Name = "TerraformServer"
+    Name = "my_vpc"
   }
 }
 
-resource "aws_vpc" "my_testvpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
   tags = {
-    Name = "MyTestVPC"
+    Name = "my_subnet"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-sivka" # Change this to a unique name
+
+  tags = {
+    Name = "my_bucket"
   }
 }
